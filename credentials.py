a4d349ba768944402fea31fad047adfca2d3c058
FernandoTM13 <85071376+FernandoTM13@users.noreply.github.com>
2023-07-29 22:53:44 -0500

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-Twilio_account_sid = 'ACefcf551af987121f0a4b05d842443804'
-Twilio_account_auth = '253f0aaea5ab55ed2c68b40019b9aae6'
-Twilio_numer = '+17624000890'
-weather_api = '57b3d9f8eb9b411eabd174056232807'
+Twilio_account_sid = 'ACefcf551af987121f0a4b05d842443xxxx'
+Twilio_account_auth = '253f0aaea5ab55ed2c68b40019b9axxx'
+Twilio_numer = '+176240xxxx'
+weather_api = '57b3d9f8eb9b411eabd17405623xxx'
